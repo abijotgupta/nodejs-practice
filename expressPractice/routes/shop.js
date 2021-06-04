926b52818b87def28e5c049c41222c3e1e54b8f0
Abijot Gupta <abijotgupta1999@gmail.com>
2021-06-04 16:34:44 +0530

    Styling Added

--- expressPractice/routes/shop.js
+++ expressPractice/routes/shop.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const path = require('path');
 const router = express.Router();
+const rootDir = require('../util/path');
 
 router.get('/', (req, res, next) => {
-    console.log('In the middleware');
-    res.sendFile(path.join(__dirname, '../', 'views', 'shop.html'));
+    res.sendFile(path.join(rootDir, 'views', 'shop.html'));
 });
 
 module.exports = router;
