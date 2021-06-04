62e36c7589cd944c13850af326d63878935a55cf
Abijot Gupta <abijotgupta1999@gmail.com>
2021-06-04 16:10:54 +0530

    Serving Html Pages

--- expressPractice/app.js
+++ expressPractice/app.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 const app = express();
 
 const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shops')
+const shopRoutes = require('./routes/shop')
 
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
+
 app.use((req, res, next) => {
-    res.send(`<h1>Error!!!!! Page not Found</h1>`)
+    res.status(400).sendFile(path.join(__dirname, 'views', '404.html'));
 })
 app.listen(5700);
--- expressPractice/routes/admin.js
+++ expressPractice/routes/admin.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const path = require('path');
 
-router.get('/add-prod', (req, res, next) => {
-    console.log('In the Add Product middleware');
-    res.send(`<form action="/admin/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button</form>`);
+router.get('/add-product', (req, res, next) => {
+    res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
 });
 
-router.post('/product', (req, res, next) => {
+router.post('/add-product', (req, res, next) => {
     console.log(req.body);
     res.redirect('/');
 });
