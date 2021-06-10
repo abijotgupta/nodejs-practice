8047a781155a17e8790f51ad5970652bbf6b2b4a
Abijot Gupta <abijotgupta1999@gmail.com>
2021-06-10 22:42:07 +0530

    Ejs Finished

--- expressPractice/app.js
+++ expressPractice/app.js
@@ -6,10 +6,10 @@ const app = express();
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop')
 
-app.set('view engine', 'pug');
+app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminData.routes);
