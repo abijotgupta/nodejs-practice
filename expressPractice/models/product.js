909ba7f6a75c0458c42838caa4e5027d78e2729d
Abijot Gupta <abijotgupta1999@gmail.com>
2021-06-12 16:34:23 +0530

    Functionalities Added

--- expressPractice/models/product.js
+++ expressPractice/models/product.js
@@ -19,16 +19,15 @@ const getProductsFromFile = (callback) => {
 }
 
 module.exports = class Product {
-    constructor(title) {
+    constructor(title, imageUrl, description, price) {
         this.title = title;
+        this.imageUrl = imageUrl;
+        this.description = description;
+        this.price = price;
     }
 
     save() {
-        // const local_path = path.join(
-        //     path.dirname(process.mainModule.filename),
-        //     'data',
-        //     'products.json'
-        // );
+        this.id = Math.random().toString();
         getProductsFromFile(products => {
             products.push(this);
             fs.writeFile(local_path, JSON.stringify(products), (err) => {
@@ -40,4 +39,4 @@ module.exports = class Product {
     static fetchAll(callback) {
         getProductsFromFile(callback);
     }
-}
+};
--- expressPractice/routes/shop.js
+++ expressPractice/routes/shop.js
@@ -1,9 +1,19 @@
+const path = require('path');
 const express = require('express');
 const router = express.Router();
 
+const shopController = require('../controllers/shop');
 
-const { getProducts } = require('../controllers/products');
+router.get('/', shopController.getIndex);
 
-router.get('/', getProducts);
+router.get('/products', shopController.getProducts);
+
+router.get('/products/:productId', shopController.getProduct);
+
+router.get('/cart', shopController.getCart);
+
+router.get('/orders', shopController.getOrders);
+
+router.get('/checkout', shopController.getCheckout);
 
 module.exports = router;
